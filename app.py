67008e26ceade7fa0ac56f6ae089758f709fc540
FernandoSLuz <fernandosilvadaluz@gmail.com>
2020-01-09 17:59:46 -0300

    Fernando

--- app.py
+++ app.py
@@ -16,6 +16,40 @@ socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
+class my_room:
+    name = ""
+    my_players = []
+    def __init__(self, name, username, user_id):
+        self.name = name
+        self.my_players.append(my_player(username, user_id))
+class my_player:
+    username = ""
+    user_id = ""
+    def __init__(self, username, user_id):
+        self.username = username
+        self.user_id = user_id
+
+my_rooms = []
+
+# def add_room():
+#     break;
+def add_player(room_name, user_id, username):
+    if(len(my_rooms) == 0):
+        my_rooms.append(my_room(user_id, username, user_id));
+        return;
+    for actual_room in my_rooms:
+        if(actual_room.name == room_name):
+            for actual_player in actual_room.my_players:
+                if(actual_player.user_id == user_id):
+                    return;
+    my_rooms.append(my_room(user_id, username, user_id));
+def remove_player(username, room_name):
+    if(len(my_rooms) > 0):
+        for actual_room in my_rooms:
+            if(actual_room.name == room_name):
+                for num, actual_player in enumerate(actual_room.my_players, start = 0):
+                    if(actual_player.username == username):
+                        actual_room.my_players.pop(num);
 
 def background_thread():
     """Example of how to send server generated events to clients."""
@@ -27,7 +61,6 @@ def background_thread():
                       {'data': 'Server generated event', 'count': count},
                       namespace='/test')
 
-
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
@@ -37,20 +70,26 @@ def index():
 def test_message(message):
     if(isinstance(message, str)):
         message = json.loads(message)
+    username = "unknown"
+    if 'username' in message:
+        username = message['username']
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+         {'data': message['data'], 'count': session['receive_count'], 'username': username})
 
 
 @socketio.on('my_broadcast_event', namespace='/haveYouHeard')
 def test_broadcast_message(message):
     if(isinstance(message, str)):
         message = json.loads(message)
+    username = "unknown"
+    if 'username' in message:
+        username = message['username']
     session['receive_count'] = session.get('receive_count', 0) + 1
     print(type(message))
     print({'data': message['data']})
     emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
+         {'data': message['data'], 'count': session['receive_count'], 'username': username},
          broadcast=True)
 
 
@@ -58,31 +97,41 @@ def test_broadcast_message(message):
 def join(message):
     if(isinstance(message, str)):
         message = json.loads(message)
+    username = "unknown"
+    if 'username' in message:
+        username = message['username']
     join_room(message['room'])
+    add_player(rooms()[1], rooms()[0], username)
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
          {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count'], 'rooms': len(rooms)})
+          'count': session['receive_count'], 'username': username})
 
 
 @socketio.on('leave', namespace='/haveYouHeard')
 def leave(message):
     if(isinstance(message, str)):
         message = json.loads(message)
+    username = "unknown"
+    if 'username' in message:
+        username = message['username']
     leave_room(message['room'])
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
          {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
+          'count': session['receive_count'], 'username': username})
 
 
 @socketio.on('close_room', namespace='/haveYouHeard')
 def close(message):
     if(isinstance(message, str)):
         message = json.loads(message)
+    username = "unknown"
+    if 'username' in message:
+        username = message['username']
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
+                         'count': session['receive_count'], 'username': username},
          room=message['room'])
     close_room(message['room'])
 
@@ -91,9 +140,12 @@ def close(message):
 def send_room_message(message):
     if(isinstance(message, str)):
         message = json.loads(message)
+    username = "unknown"
+    if 'username' in message:
+        username = message['username']
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
+         {'data': message['data'], 'count': session['receive_count'], 'username': username},
          room=message['room'])
 
 
